311452e8460f39fedeb936a0fcd5fdeb05405e49
Admin <Administrator@THINKPALM.LAN>
2020-08-28 22:52:16 +0530

    updated dependencies of server package.json

---
+++ client/build/precache-manifest.29d00e69749d942d5ea1982989879413.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be69e60f02bea929e2f2dd895aa5bdb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "657936b47ed1167bc474",
+    "url": "/static/css/main.f4307d55.chunk.css"
+  },
+  {
+    "revision": "7329348a4c4d2245404d",
+    "url": "/static/js/2.67e52299.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.67e52299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "657936b47ed1167bc474",
+    "url": "/static/js/main.b314ddf8.chunk.js"
+  },
+  {
+    "revision": "10c44fc958f8ca86a165",
+    "url": "/static/js/runtime-main.9dad3f27.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
