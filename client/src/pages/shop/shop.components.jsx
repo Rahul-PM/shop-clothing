03912b2499a6b03b42135472e4a84fb2a92a2a93
Rahul P M <rahul.p@thinkpalm.com>
2020-09-01 18:47:21 +0530

    added optimisation using usecallback , memo and added  compression

--- client/src/pages/shop/shop.components.jsx
+++ client/src/pages/shop/shop.components.jsx
@@ -1,17 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
-import CollectionPageContainer from '../../pages/collection/collection.container';
+import ErrorBoundary from '../../components/error-boundary/error-boundary.component';
 
 import { selectedIsCollectionLoaded } from '../../redux/shop/shop.selector';
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
+import Spinner from '../../components/spinner/spinner.component';
 
+const CollectionsOverviewContainer = lazy(() =>
+    import('../../components/collections-overview/collections-overview.container'));
 
-const ShopPage = ({ match,fetchCollectionsStart }) => {
+const CollectionPageContainer = lazy(() =>
+    import('../../pages/collection/collection.container'));
+
+const ShopPage = ({ match, fetchCollectionsStart }) => {
 
     useEffect(() => {
         fetchCollectionsStart();
@@ -19,10 +23,14 @@ const ShopPage = ({ match,fetchCollectionsStart }) => {
 
     return (
         <div className='shop-page'>
-            <Route exact path={`${match.path}`} component={CollectionsOverviewContainer}
-            />
-            <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer}
-            />
+            <ErrorBoundary>
+                <Suspense fallback={<Spinner />}>
+                    <Route exact path={`${match.path}`} component={CollectionsOverviewContainer}
+                    />
+                    <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer}
+                    />
+                </Suspense>
+            </ErrorBoundary>
         </div>
     );
 
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express  = require('express');
+const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const compression = require('compression');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -10,6 +11,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -43,50 +45,3 @@ app.post('/payment', (req, res) => {
         }
     })
 });
-
-
-// const express = require('express');
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
-// const path = require('path');
-
-// if (process.env.NODE_ENV !== 'production') require('dotenv').config();
-
-// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.use(cors());
-
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, 'client/build')));
-
-//   app.get('*', function(req, res) {
-//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-//   });
-// }
-
-// app.listen(port, error => {
-//   if (error) throw error;
-//   console.log('Server running on port ' + port);
-// });
-
-// app.post('/payment', (req, res) => {
-//   const body = {
-//     source: req.body.token.id,
-//     amount: req.body.amount,
-//     currency: 'usd'
-//   };
-
-//   stripe.charges.create(body, (stripeErr, stripeRes) => {
-//     if (stripeErr) {
-//       res.status(500).send({ error: stripeErr });
-//     } else {
-//       res.status(200).send({ success: stripeRes });
-//     }
-//   });
-// });
