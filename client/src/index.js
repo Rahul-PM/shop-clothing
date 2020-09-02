d93c7d275e835b694f1a23a0bf2983d84444d69b
Rahul P M <rahul.p@thinkpalm.com>
2020-09-02 12:58:24 +0530

    added pwa ,and modified manifest and added express sslify for https

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,8 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 
+import * as serviceWorker from './serviceWorker';
+
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
@@ -20,3 +22,5 @@ ReactDOM.render(
   ,
   document.getElementById('root')
 );
+
+serviceWorker.register();
